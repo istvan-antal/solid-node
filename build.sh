6c9e6129b95c1bfcde0bf5fac46d97903de7d21c
István Antal <istvan.m.antal@gmail.com>
2015-12-03 13:36:04 +0000

    Renamed:    main.pp -> system.pp

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get install puppet
 cd "$(dirname ${BASH_SOURCE[0]})"
-puppet module install puppetlabs-nodejs --modulepath=./modules
-sudo puppet apply --verbose --debug --modulepath=./modules main.pp
+puppet module install puppetlabs-nodejs --modulepath=./system
+sudo puppet apply --verbose --debug --modulepath=./system system.pp
